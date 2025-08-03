21ca5d866b87736041833e74c8b33eb4c8564612
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-08-03 20:43:11 +0330

    remove some extra lines from food.py

--- SnakeGame/food.py
+++ SnakeGame/food.py
@@ -6,8 +6,3 @@ def food_coordinate(width, height, step):
     global FOODS
     food_imoji = np.random.choice(FOODS)
     return (np.random.choice(range(0, width, step)), np.random.choice(range(0, height, step)), food_imoji)
-    global xx 
-    xx += 24
-    if xx > height - 100:
-        xx = 240
-    return (360, xx)
